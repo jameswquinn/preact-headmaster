3d067e677e06a74b89e1962a7450d417489e45d3
jameswquinn <jameswquinn@users.noreply.github.com>
2024-08-17 22:24:12 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,1 +1,23 @@
-placeholder
+import { defineConfig } from 'vite';
+import preact from '@preact/preset-vite';
+import { resolve } from 'path';
+import pkg from './package.json';
+
+export default defineConfig({
+  plugins: [preact()],
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: 'PreactHeadmaster',
+      fileName: (format) => `preact-headmaster.${format}.js`
+    },
+    rollupOptions: {
+      external: [...Object.keys(pkg.peerDependencies || {})],
+      output: {
+        globals: {
+          preact: 'preact'
+        }
+      }
+    }
+  }
+});
